432d91de5e87cd08aab6ffa0494dd9b4f7ce7f38
Audrey Sengelin <audrey.sengelin@gmail.com>
2019-08-28 17:38:40 -0700

    I created a variable math.floor math.random for the computer choices. I also created a variable with the number of choices left.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,19 +4,32 @@ var letter = document.getElementById("letter");
 var wins = document.getElementById("wins");
 var left = document.getElementById("left");
 var lettersTried = document.getElementById("lettersTried");
-
-
+var choicesLeft = 10;
 
 document.onkeyup = function(event) {
-    console.log(event.key)
+    console.log(
+        " Event Key is " + event.key + " The computer choice's was " + computerLetter
+    )
     letter.textContent = event.key
 
     if (event.key === computerLetter) {
         console.log("Correct");
+        choicesLeft = choicesLeft - 1;
+        wins = wins + 1;
     }
     if (event.key !== computerLetter) {
         console.log("Wrong");
+        choicesLeft = choicesLeft - 1;
+        losses = losses - 1;
     }
+    console.log(choicesLeft);
 }
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// console.log(computerLetter);
+
+
+
+// var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
